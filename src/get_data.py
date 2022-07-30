b9e05a9cea3bc242aee402010757ce82f01793c4
Ben Erichson <erichson@pitt.edu>
2022-07-29 19:44:15 -0600

    updates

--- src/get_data.py
+++ src/get_data.py
@@ -6,11 +6,11 @@ from torch.utils.data import Dataset, DataLoader, TensorDataset
 #******************************************************************************
 def getData(name, train_bs=128, test_bs=256):
     if name == 'isoflow':
-       train_loader = DataLoader(torch.load('datasets/isoflow/isoflow_train_s4.npy'), batch_size=train_bs, shuffle=True)
-       test1_loader = DataLoader(torch.load('datasets/isoflow/isoflow_test_1_s4.npy'), batch_size=test_bs, shuffle=False)
-       val1_loader = DataLoader(torch.load('datasets/isoflow/isoflow_val_1_s4.npy'), batch_size=test_bs, shuffle=False)
-       test2_loader = DataLoader(torch.load('datasets/isoflow/isoflow_test_2_s4.npy'), batch_size=test_bs, shuffle=False)
-       val2_loader = DataLoader(torch.load('datasets/isoflow/isoflow_val_2_s4.npy'), batch_size=test_bs, shuffle=False)
+       train_loader = DataLoader(torch.load('datasets/isoflow/isoflow_train_s8.npy'), batch_size=train_bs, shuffle=True)
+       test1_loader = DataLoader(torch.load('datasets/isoflow/isoflow_test_1_s8.npy'), batch_size=test_bs, shuffle=False)
+       val1_loader = DataLoader(torch.load('datasets/isoflow/isoflow_val_1_s8.npy'), batch_size=test_bs, shuffle=False)
+       test2_loader = DataLoader(torch.load('datasets/isoflow/isoflow_test_2_s8.npy'), batch_size=test_bs, shuffle=False)
+       val2_loader = DataLoader(torch.load('datasets/isoflow/isoflow_val_2_s8.npy'), batch_size=test_bs, shuffle=False)
        return train_loader, test1_loader, val1_loader, test2_loader, val2_loader
 
     if name == 'DoubleGyre':
