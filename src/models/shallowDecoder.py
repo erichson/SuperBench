e52643719dd0074cf424d7785e996c49d47f9f38
Ben Erichson <erichson@pitt.edu>
2022-07-29 19:41:29 -0600

    updates

--- src/models/shallowDecoder.py
+++ src/models/shallowDecoder.py
@@ -16,10 +16,10 @@ class shallowDecoder(nn.Module):
         self.input_size = input_size
         self.output_size = output_size
         
-        self.l1 = nn.ConvTranspose2d(in_channels=1, out_channels=64, kernel_size=5, stride=2, padding=1)
-        self.l2 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1)
-        self.l3 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=1, stride=1, dilation=1)
-        self.l4 = nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=2, stride=1, dilation=1)
+        self.l1 = nn.ConvTranspose2d(in_channels=1, out_channels=128, kernel_size=5, stride=2, padding=1)
+        self.l2 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1)
+        self.l3 = nn.ConvTranspose2d(in_channels=128, out_channels=32, kernel_size=3, stride=1, dilation=1)
+        self.l4 = nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=3, stride=1, dilation=1)
 
     def forward(self, x):
         t,c,m,n = x.shape
--- train.py
+++ train.py
@@ -66,7 +66,7 @@ model = torch.nn.DataParallel(model)
 #==============================================================================
 print(model)    
 print('**** Setup ****')
-print('Total params Generator: %.7fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
+print('Total params Generator: %.3fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
 print('************')    
 
 
